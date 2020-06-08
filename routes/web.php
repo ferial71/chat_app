d0688d98fc34891182741463f5944618d6b96210
ferial71 <ff_bouzoul@esi.dz>
2020-06-08 15:13:48 +0100

    fff

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+use Symfony\Component\Routing\Annotation\Route;
+
+Route::get('/{any}', function () {
+    return view('layouts.master');
+})->where('any','.*');
+
+Route::post('/register', 'UserController@register')->middleware('guest');
+Route::post('/login', 'UserController@login')->middleware('guest');
+Route::post('/update/token', 'UserController@updateToken');
